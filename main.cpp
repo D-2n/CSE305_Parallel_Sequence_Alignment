a6f53699340903ee9995fe5fddd9d6210be1f1fa
bogdanaKolic <bogdana.kolic@gmail.com>
2024-06-10 21:44:30 +0200

    Parallel Prefix

--- main.cpp
+++ main.cpp
@@ -2,83 +2,184 @@
 #include <thread>
 #include <vector>
 #include <math.h>
+#include <deque>
 
 typedef struct alignment_point {
-    int i;
-    int j;
-    int t;
+    size_t i;
+    size_t j;
+    size_t t;
     struct alignment_point* next = NULL;
 } align;
 
 void OptimalAlignmentMapThread(){
-    // to complete
+    // to compete
 }
 
-void optimal_alignment(std::vector<align> partial_bp, int n, int m, int p) {
-    std::vector<int> omega(0);
-    std::vector<int> partial_sums(0);
+typedef struct parallel_prefix_queue_element {
+    size_t value;
+    size_t begin_id;
+    size_t end_id;
+    struct parallel_prefix_queue_element* next;
+} queue_indices;
+
+void PrefixSumMapThread(std::vector<size_t> &sums, size_t value, queue_indices *curr) {
+    if (curr != NULL) {
+        for (size_t i = curr->begin_id; i < curr->end_id; i++) {
+            sums[i] += value;
+        }
+    }
+}
+
+void PrefixInitMapThread(std::vector<size_t> &values, std::vector<size_t> &sums, queue_indices &q) {
+    sums[q.begin_id] = values[q.begin_id];
+    for (size_t i = q.begin_id + 1; i < q.end_id; i++) {
+        sums[i] = sums[i-1] + values[i];
+    }
+    q.value = sums[q.end_id - 1];
+}
+
+void ParallelPrefix(size_t p, std::vector<size_t> &values, std::vector<size_t> &partial_sums) {
+
+    std::vector<queue_indices> units;
+    std::deque<queue_indices*> processes;
+
+    // initialization of the elements
+    size_t n = values.size();
+    size_t block_size = n / p;
+    if (p > n) {
+        block_size = 1;
+    }
+    for (size_t i = 0; i < p; i++) {
+        queue_indices q;
+        q.value = 0;
+        q.begin_id = block_size * i;
+        q.next = NULL;
+        q.end_id = block_size * (i + 1);
+        if (q.end_id >= n) {
+            q.end_id = n;
+            units.push_back(q);
+            break;
+        }
+        units.push_back(q);
+    }
+
+
+    // suming the elements of p partitions
+    size_t num_partitions = units.size();
+
+    std::vector<std::thread> workers(num_partitions-1);
+    for (size_t i = 1; i < num_partitions; i++) {
+        units[i-1].next = &(units[i]);
+        workers[i-1] = std::thread(&PrefixInitMapThread, std::ref(values), std::ref(partial_sums), std::ref(units[i]));
+    }
+    PrefixInitMapThread(values, partial_sums, units[0]);
+    for (size_t i = 0; i < num_partitions - 1; i++) {
+        processes.push_back(&(units[i]));
+        workers[i].join();
+    }
+    processes.push_back(&(units[num_partitions - 1]));
+
+    // pointer-jumping
+    while (!processes.empty()) {
+        size_t m = processes.size();
+        std::vector<std::thread> workers(0);
+        std::deque<queue_indices*> aux(m-1);
+        for (size_t i = 0; i < m-1; i++) {
+            queue_indices *q = processes.front();
+            processes.pop_front();
+            if(q->next != NULL) {
+                queue_indices* r = q->next;
+                q->next = r->next;
+                q->value = partial_sums[q->end_id - 1];
+                workers.push_back(std::thread(&PrefixSumMapThread, std::ref(partial_sums), q->value, r));
+                processes.push_back(q);
+            }
+        }
+        
+        queue_indices *q = processes.front();
+        processes.pop_front();
+        PrefixSumMapThread(partial_sums, q->value, q->next);
+
+        for(size_t i = 0; i < workers.size(); i++) {
+            workers[i].join();
+        }
+        
+    }
+
+}
+
+void optimal_alignment(std::vector<align> partial_bp, size_t n, size_t m, size_t p) {
+    size_t num_subproblems = partial_bp.size() - 1;
+    std::vector<size_t> omega(0);
+    std::vector<size_t> partial_sums(num_subproblems);
     std::vector<align>::iterator begin = partial_bp.begin();
     std::vector<align>::iterator end = partial_bp.end();
-    int i = 0;
+    size_t i = 0;
     while (begin + 1 != end) {
-        int a = std::ceil(((begin+1)->i - begin->i) * p * 1.0 / m);
-        int b = std::ceil(((begin+1)->j - begin->j) * p * 1.0 / n);
+        size_t a = std::ceil(((begin+1)->i - begin->i) * p * 1.0 / m);
+        size_t b = std::ceil(((begin+1)->j - begin->j) * p * 1.0 / n);
         omega.push_back(std::max(a, b));
         begin++;
+        /* 
         if (i == 0){
             partial_sums.push_back(omega[0]);
         }
         else{
             partial_sums.push_back(omega[i] + partial_sums[i-1]);
-        }
+        }*/
         i++;
     }
     // add parallel prefix for partial sum
+    ParallelPrefix(p, omega, partial_sums);
 
-    /*for(int j=0; j<omega.size();j++){
+    /* testing 
+    for(size_t j=0; j<omega.size();j++){
         printf("%d ", omega[j]);
     }
     printf("\n");
-    for(int j=0; j<partial_sums.size();j++){
+    for(size_t j=0; j<partial_sums.size();j++){
         printf("%d ", partial_sums[j]);
     }
-    printf("\n");*/
+    printf("\n");
+    */
     
+    /* solving subproblems
     // solve subproblems 0, 3, ...
     // solve subproblems 1, 4, ...
     // solve subproblems 2, 5, ...
-    int num_suproblems = omega.size();
-    std::vector<int> num_proc_per_sub(0); // to be computed after parallel-prefix
+    size_t num_subproblems = omega.size();
+    std::vector<size_t> num_proc_per_sub(0); // to be computed after parallel-prefix
     std::vector<std::thread> workers(num_subproblems - 1);
     std::vector<align*> pointers (num_subproblems * 2); // beginning-end
 
-    for (int i=0; i<num_subproblems - 1; i+=3) {
+    for (size_t i=0; i<num_subproblems - 1; i+=3) {
         workers[i] = std::thread(&OptimalAlignmentMapThread, ...)
     }
     OptimalAlignmentMapThread(...);
-    for(int i=0; i<num_subproblems - 1; i++) {
+    for(size_t i=0; i<num_subproblems - 1; i++) {
         workers[i].join();
     }
-    for (int i=1; i<num_subproblems - 1; i+=3) {
+    for (size_t i=1; i<num_subproblems - 1; i+=3) {
         workers[i] = std::thread(&OptimalAlignmentMapThread, ...)
     }    
     OptimalAlignmentMapThread(...);
-    for(int i=0; i<num_subproblems - 1; i++) {
+    for(size_t i=0; i<num_subproblems - 1; i++) {
         workers[i].join();
     }
-    for (int i=2; i<num_subproblems - 1; i+=3) {
+    for (size_t i=2; i<num_subproblems - 1; i+=3) {
         workers[i] = std::thread(&OptimalAlignmentMapThread, ...)
     }
     OptimalAlignmentMapThread(...);
-    for(int i=0; i<num_subproblems - 1; i++) {
+    for(size_t i=0; i<num_subproblems - 1; i++) {
         workers[i].join();
     }
     // concatenate the results
-    for(int i = 1; i<num_subproblems-1; i++) {
+    for(size_t i = 1; i<num_subproblems-1; i++) {
         align* prev = pointers[2*(i-1)+1];
         align* next = pointers[2*i];
         prev -> next = next;
     }
+    */
 }
 
 int main(){
