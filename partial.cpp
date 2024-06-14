56af62319dfe8bb740941fe717e1da85d5f1b146
safaa-40 <115423854+safaa-40@users.noreply.github.com>
2024-06-14 14:40:53 +0200

    Update partial.cpp

--- partial.cpp
+++ partial.cpp
@@ -1,16 +1,17 @@
-#include <vector>
 #include <iostream>
+#include <vector>
 #include <cmath>
 #include <algorithm>
+#include <tuple>
 
 using namespace std;
 
-struct align {
+typedef struct alignment_point {
     size_t i;
     size_t j;
     int t;
-    align* next = nullptr;
-};
+    struct alignment_point* next = nullptr;
+} align;
 
 const double MATCH_SCORE = 2;
 const double MISMATCH_PENALTY = 0;
@@ -44,54 +45,66 @@ void fill_tables(const string& A, const string& B, vector<vector<double>>& T1, v
     }
 }
 
-void traceback_path(const vector<vector<double>>& T1, const vector<vector<double>>& T2, const vector<vector<double>>& T3, vector<align>& partial_bp) {
+void traceback_path(const vector<vector<double>>& T1, const vector<vector<double>>& T2, const vector<vector<double>>& T3, vector<align>& partial_bp, int start_i, int start_j) {
     int m = T1.size() - 1;
     int n = T1[0].size() - 1;
     int i = m, j = n;
 
     while (i > 0 && j > 0) {
         if (T1[i][j] >= T2[i][j] && T1[i][j] >= T3[i][j]) {
-            size_t i_ = i;
-            size_t j_ = j;
+            size_t i_ = start_i + i;
+            size_t j_ = start_j + j;
             partial_bp.push_back({i_, j_, 1});
             --i;
             --j;
         } else if (T2[i][j] >= T1[i][j] && T2[i][j] >= T3[i][j]) {
-            size_t i_ = i;
-            size_t j_ = j;
+            size_t i_ = start_i + i;
+            size_t j_ = start_j + j;
             partial_bp.push_back({i_, j_, 2});
             --j;
         } else {
-            size_t i_ = i;
-            size_t j_ = j;
+            size_t i_ = start_i + i;
+            size_t j_ = start_j + j;
             partial_bp.push_back({i_, j_, 3});
             --i;
         }
     }
-    partial_bp.push_back({0, 0, -1}); // Add start point
+    partial_bp.push_back({start_i, start_j, 0}); // Add start point with a special value for t
     reverse(partial_bp.begin(), partial_bp.end());
 }
 
-void optimal_partition(const string& A, const string& B, vector<align>& partial_bp) {
+// This function computes the partition points for the sequences based on the number of processors.
+vector<align> compute_partitions(const string& A, const string& B, int num_partitions) {
     int m = A.length();
     int n = B.length();
-    
-    vector<vector<double>> T1, T2, T3;
-    initialize_tables(T1, T2, T3, m, n);
-    fill_tables(A, B, T1, T2, T3);
-    traceback_path(T1, T2, T3, partial_bp);
+    vector<align> partitions;
+    int step_i = m / num_partitions;
+    int step_j = n / num_partitions;
+
+    for (int t = 0; t < num_partitions; ++t) {
+        int start_i = t * step_i;
+        int end_i = (t == num_partitions - 1) ? m : (t + 1) * step_i - 1;
+        int start_j = t * step_j;
+        int end_j = (t == num_partitions - 1) ? n : (t + 1) * step_j - 1;
+        
+        partitions.push_back({start_i, start_j, 0});
+        partitions.push_back({end_i, end_j, 0});
+    }
+
+    return partitions;
 }
 
 int main() {
     string A = "ATGTCGA";
     string B = "AGAATCTA";
+    
+    int num_partitions = 4; // Example number of partitions
+    vector<align> partitions = compute_partitions(A, B, num_partitions);
 
-    vector<align> partial_bp;
-    optimal_partition(A, B, partial_bp);
-
-    for (const auto& point : partial_bp) {
-        cout << "(" << point.i << ", " << point.j << ", " << point.t << ")\n";
+    for (const auto& partition : partitions) {
+        cout << "Partition: (" << partition.i << ", " << partition.j << ", " << partition.t << ")\n";
     }
 
     return 0;
 }
+
