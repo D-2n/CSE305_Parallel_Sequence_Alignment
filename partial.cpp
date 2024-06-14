e1dce0495f4adb91326a5fbf2fa6a3389c815ffd
safaa-40 <115423854+safaa-40@users.noreply.github.com>
2024-06-14 16:43:11 +0200

    Update partial.cpp

--- partial.cpp
+++ partial.cpp
@@ -1,114 +1,144 @@
 #include <iostream>
 #include <vector>
 #include <thread>
-#include <mutex>
-#include <algorithm>
 #include <cmath>
-#include <climits>
+#include <algorithm>
 
 using namespace std;
 
-struct Cell {
-    int i, j, type;
-};
+typedef struct alignment_point {
+    size_t i;
+    size_t j;
+    int t;
+    struct alignment_point* next = NULL;
+} align;
 
-struct Subproblem {
-    int i0, j0, i1, j1;
-    int startType, endType;
-};
-
-mutex mtx;
-
-// Example scoring and gap penalty functions
-int score(char a, char b) {
-    return (a == b) ? 2 : -1;
-}
-
-int gapPenalty(int k) {
-    return 2 + k;
-}
-
-// Function to initialize tables
-void initializeTables(vector<vector<int>>& T1, vector<vector<int>>& T2, vector<vector<int>>& T3, int m, int n, int startType) {
-    for (int i = 0; i <= m; ++i) {
-        for (int j = 0; j <= n; ++j) {
-            T1[i][j] = T2[i][j] = T3[i][j] = INT_MIN;
+void fill_tables_parallel(char *A, char *B, size_t m, size_t n, vector<vector<int>> &T1, vector<vector<int>> &T2, vector<vector<int>> &T3, double g, double h, size_t p) {
+    auto fill_chunk = [&](size_t start_row, size_t end_row) {
+        for (size_t i = start_row; i <= end_row; i++) {
+            for (size_t j = 1; j <= n; j++) {
+                T1[i][j] = max({T1[i-1][j-1], T2[i-1][j-1], T3[i-1][j-1]}) + (A[i-1] == B[j-1] ? 0 : 1);
+                T2[i][j] = max({T1[i][j-1] - g - h, T2[i][j-1] - g, T3[i][j-1] - g - h});
+                T3[i][j] = max({T1[i-1][j] - g - h, T2[i-1][j] - g - h, T3[i-1][j] - g});
+            }
         }
+    };
+
+    vector<thread> threads;
+    size_t chunk_size = m / p;
+    for (size_t i = 0; i < p; i++) {
+        size_t start_row = i * chunk_size + 1;
+        size_t end_row = (i == p - 1) ? m : (i + 1) * chunk_size;
+        threads.emplace_back(fill_chunk, start_row, end_row);
     }
-    T1[0][0] = T2[0][0] = T3[0][0] = 0;
-}
 
-// Function to fill the tables
-void fillTables(vector<vector<int>>& T1, vector<vector<int>>& T2, vector<vector<int>>& T3, const string& A, const string& B) {
-    int m = A.size();
-    int n = B.size();
-    for (int i = 1; i <= m; ++i) {
-        for (int j = 1; j <= n; ++j) {
-            T1[i][j] = score(A[i-1], B[j-1]) + max({T1[i-1][j-1], T2[i-1][j-1], T3[i-1][j-1]});
-            T2[i][j] = max({T1[i][j-1] - gapPenalty(1), T2[i][j-1] - 1, T3[i][j-1] - gapPenalty(1)});
-            T3[i][j] = max({T1[i-1][j] - gapPenalty(1), T2[i-1][j] - gapPenalty(1), T3[i-1][j] - 1});
-        }
+    for (auto &t : threads) {
+        t.join();
     }
 }
 
-// Function to find the partial balanced partition
-void findPartition(vector<Subproblem>& partitions, const string& A, const string& B, int p) {
-    int m = A.size();
-    int n = B.size();
+vector<align> find_partition_parallel(vector<vector<int>> &T1, vector<vector<int>> &T2, vector<vector<int>> &T3, size_t m, size_t n, size_t p) {
+    vector<align> partition;
+
+    size_t block_size_m = m / p;
+    size_t block_size_n = n / p;
+
+    partition.push_back({0, 0, -1, nullptr});
+
+    auto find_max_in_block = [&](size_t i, align &max_align) {
+        size_t row = i * block_size_m;
+        size_t col = i * block_size_n;
+
+        int max_score = -1;
+
+        for (size_t j = 1; j < n; j++) {
+            if (T1[row][j] > max_score) {
+                max_score = T1[row][j];
+                max_align = {row, j, 1, nullptr};
+            }
+            if (T2[row][j] > max_score) {
+                max_score = T2[row][j];
+                max_align = {row, j, 2, nullptr};
+            }
+            if (T3[row][j] > max_score) {
+                max_score = T3[row][j];
+                max_align = {row, j, 3, nullptr};
+            }
+        }
+
+        for (size_t k = 1; k < m; k++) {
+            if (T1[k][col] > max_score) {
+                max_score = T1[k][col];
+                max_align = {k, col, 1, nullptr};
+            }
+            if (T2[k][col] > max_score) {
+                max_score = T2[k][col];
+                max_align = {k, col, 2, nullptr};
+            }
+            if (T3[k][col] > max_score) {
+                max_score = T3[k][col];
+                max_align = {k, col, 3, nullptr};
+            }
+        }
+    };
+
+    vector<thread> threads;
+    vector<align> max_aligns(p);
+
+    for (size_t i = 1; i < p; i++) {
+        threads.emplace_back(find_max_in_block, i, ref(max_aligns[i]));
+    }
+
+    for (auto &t : threads) {
+        t.join();
+    }
+
+    for (const auto &max_align : max_aligns) {
+        partition.push_back(max_align);
+    }
+
+    partition.push_back({m, n, 1, nullptr});
+    return partition;
+}
+
+void find_partial_balanced_partition_parallel(char *A, char *B, size_t m, size_t n, size_t p, double g, double h, vector<align> &partition) {
     vector<vector<int>> T1(m + 1, vector<int>(n + 1));
     vector<vector<int>> T2(m + 1, vector<int>(n + 1));
     vector<vector<int>> T3(m + 1, vector<int>(n + 1));
-
-    initializeTables(T1, T2, T3, m, n, -1);
-    fillTables(T1, T2, T3, A, B);
-
-    int rowPartSize = m / p;
-    int colPartSize = n / p;
-
-    for (int i = 0; i < p; ++i) {
-        int i0 = i * rowPartSize;
-        int i1 = (i + 1) * rowPartSize - 1;
-        int j0 = i * colPartSize;
-        int j1 = (i + 1) * colPartSize - 1;
-
-        int startType = (i == 0) ? -1 : (i % 3) + 1;
-        int endType = ((i + 1) % 3) + 1;
-
-        partitions.push_back({i0, j0, i1, j1, startType, endType});
+    
+    // Initialize the tables
+    for (size_t i = 0; i <= m; i++) {
+        T1[i][0] = -1;
+        T2[i][0] = -1;
+        T3[i][0] = -1;
     }
-}
-
-// Thread function to process subproblems
-void processSubproblems(vector<Subproblem>& partitions, int threadId, int p) {
-    int subproblemSize = partitions.size() / p;
-    int start = threadId * subproblemSize;
-    int end = (threadId + 1) * subproblemSize;
-
-    for (int i = start; i < end; ++i) {
-        // Processing the subproblem (for demonstration, just print the details)
-        mtx.lock();
-        cout << "Thread " << threadId << " processing subproblem: (" << partitions[i].i0 << "," << partitions[i].j0 << ") to ("
-             << partitions[i].i1 << "," << partitions[i].j1 << "), startType: " << partitions[i].startType << ", endType: " << partitions[i].endType << endl;
-        mtx.unlock();
+    for (size_t j = 0; j <= n; j++) {
+        T1[0][j] = -1;
+        T2[0][j] = -1;
+        T3[0][j] = -1;
     }
+    T1[0][0] = 0;
+
+    fill_tables_parallel(A, B, m, n, T1, T2, T3, g, h, p);
+    partition = find_partition_parallel(T1, T2, T3, m, n, p);
 }
 
 int main() {
-    int p = 4; // Number of processors
-    string A = "ATGTCGA";
-    string B = "AGAATCTA";
-
-    vector<Subproblem> partitions;
-    findPartition(partitions, A, B, p);
-
-    vector<thread> threads;
-    for (int i = 0; i < p; ++i) {
-        threads.push_back(thread(processSubproblems, ref(partitions), i, p));
+    char A[] = "AGTACGCA";
+    char B[] = "TATGC";
+    size_t m = 8;
+    size_t n = 5;
+    size_t p = 4;
+    double g = 1.0;
+    double h = 2.0;
+    
+    vector<align> partition;
+    find_partial_balanced_partition_parallel(A, B, m, n, p, g, h, partition);
+    
+    for (const auto &align : partition) {
+        cout << "(" << align.i << ", " << align.j << ", " << align.t << ")\n";
     }
-
-    for (auto& th : threads) {
-        th.join();
-    }
-
+    
     return 0;
 }
+
